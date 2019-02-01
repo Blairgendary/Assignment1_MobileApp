d02b3264963b3fd530e3e2d21534267344e213e8
Blair <m.blair@uleth.ca>
2019-01-31 17:14:25 -0700

    Framework7 - Added Toolbars and Panels/Pages

--- platforms/browser/www/js/script.js
+++ platforms/browser/www/js/script.js
@@ -1,5 +1,11 @@
 var app = new Framework7({
-    root: "#app";
-})
+    root: "#app",
+    routes: [
+        {
+            path: '/page2/',
+            url: 'pages/page2.html'
+        }
+    ]
+});
 
 var mainView = app.views.create('.view-main');
